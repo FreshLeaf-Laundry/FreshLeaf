4d00eedcecec25eb60c76b61a935dd63c46eb430
Katon Bagaskoro <bagaskoro55@gmail.com>
2024-12-19 12:29:10 +0700

    buat migration dan seeding user

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User; // Pastikan untuk mengimpor model User
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Membuat akun dengan role admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'), 
+            'role' => 'admin',
+        ]);
+
+        // Membuat akun dengan role user
+        User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user123'), 
+            'role' => 'user',
+        ]);
+    }
+}
